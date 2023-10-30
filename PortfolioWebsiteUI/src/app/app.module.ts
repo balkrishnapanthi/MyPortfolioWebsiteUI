05481a7fc829e02d05362c9f9fe18bc525092b86
SachinExt <panthisachin22@gmail.com>
2023-10-29 22:45:13 -0700

    added header and section

--- PortfolioWebsiteUI/src/app/app.module.ts
+++ PortfolioWebsiteUI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MiscComponent } from './misc/misc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MiscComponent
   ],
   imports: [
     BrowserModule,
