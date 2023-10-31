d91ca1e4881a52ba0323045f75e3af81b49dfc32
SachinExt <panthisachin22@gmail.com>
2023-10-30 22:30:13 -0700

    added footer

--- PortfolioWebsiteUI/src/app/app.module.ts
+++ PortfolioWebsiteUI/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MiscComponent } from './misc/misc.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MiscComponent
+    MiscComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
